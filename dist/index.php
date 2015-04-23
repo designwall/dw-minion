73190f88ad71f044327f0ba68dc10bcea4c2bbb4
Pumpkjn <Pumpkjn1508@gmail.com>
2015-04-23 09:11:17 +0700

    update

--- dist/index.php
+++ dist/index.php
@@ -11,34 +11,7 @@
 		<?php  if( is_archive() ) : ?>
 			<header class="page-header">
 						<h1 class="page-title">
-			<?php 	if ( is_category() ) :
-						printf( __( 'Category Archives: %s', 'dw-minion' ), single_cat_title( '', false ) );
-					elseif ( is_tag() ) :
-						printf( __( 'Tag Archives: %s', 'dw-minion' ), single_tag_title( '', false ) );
-					elseif ( is_author() ) :
-						the_post();
-						printf( __( 'Author: %s', 'dw-minion' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
-						rewind_posts();
-					elseif ( is_day() ) :
-						printf( __( 'Day: %s', 'dw-minion' ), '<span>' . get_the_date() . '</span>' );
-					elseif ( is_month() ) :
-						printf( __( 'Month: %s', 'dw-minion' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
-					elseif ( is_year() ) :
-						printf( __( 'Year: %s', 'dw-minion' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
-					elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-						_e( 'Asides', 'dw-minion' );
-					elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-						_e( 'Images', 'dw-minion');
-					elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-						_e( 'Videos', 'dw-minion' );
-					elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-						_e( 'Quotes', 'dw-minion' );
-					elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-						_e( 'Links', 'dw-minion' );
-					else :
-						_e( 'Archives', 'dw-minion' );
-					endif;
-				?>
+			<?php the_archive_title(); ?>
 			</h1>
 			<?php
 				$term_description = term_description();
@@ -47,6 +20,10 @@
 				endif;
 			?>
 		</header>
+		<?php elseif( is_search() ) : ?>
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'dw-minion' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+			</header>
 		<?php endif; ?>
 				<?php 
 					if ( have_posts() ) : 

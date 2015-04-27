569d96eb51e73342f65690200c506bc5779c7b0b
Pumpkjn <Pumpkjn1508@gmail.com>
2015-04-27 16:56:19 +0700

    up

--- dist/functions.php
+++ dist/functions.php
@@ -10,11 +10,6 @@ if ( ! function_exists( 'dw_minion_setup' ) ) {
 		add_theme_support( 'post-thumbnails' );
 		add_editor_style();
 		add_action( 'tgmpa_register', 'alx_plugins' );
-		add_theme_support( "title-tag" );
-		$custom_header_args = array();
-		$custom_background_args = array();
-		add_theme_support( "custom-header", $custom_header_args );
-		add_theme_support( "custom-background", $custom_background_args );
 
 		add_action( 'wp_head', 'minion_features_image_as_og_image' );
 		add_action( 'widgets_init', 'dw_minion_widgets_init' );
